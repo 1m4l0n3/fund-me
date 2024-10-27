42d07054a5f14dbb79863b39ebae6bc80b7c2b22
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-10-28 01:12:45 +0530

    [Johny] Add MockV3Aggregator contract | Update getAnvilEthConfig function

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.18;
 
-contract HelperConfig {
+import {MockV3Aggregator} from "../test/mocks/MockV3AAggregator.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
     struct NetworkConfig {
         address priceFeedAddress;
     }
 
     NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000E8;
 
     constructor() {
         if (block.chainid == 11155111) {
@@ -23,5 +28,18 @@ contract HelperConfig {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        MockV3Aggregator mockV3Aggregator = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeedAddress: address(mockV3Aggregator)
+        });
+
+        return anvilConfig;
+    }
 }
