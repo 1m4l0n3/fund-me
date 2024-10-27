9d5feb4af0911697639c076702fd7d0620c37edb
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-10-27 12:29:57 +0530

    [Johny] Add getVersion function for priceFeed

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();
@@ -44,5 +45,12 @@ contract FundMe {
             value: address(this).balance
         }("");
         require(withdrawSuccess, "Withdraw Failed!");
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.version();
     }
 }
