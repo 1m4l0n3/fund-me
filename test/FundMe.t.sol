3f65888fe62a302dfc5ffe34cd95ea3d8bf21401
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-10-27 14:24:23 +0530

    [Johny] Refactor Deploy & Test scripts

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -3,14 +3,17 @@ pragma solidity 0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FuneMeTest is Test {
     FundMe public fundMe;
+    DeployFundMe public deployFundMe;
     address private s_priceFeedAddress =
         0x694AA1769357215DE4FAC081bf1f309aDC325306;
 
     function setUp() public {
-        fundMe = new FundMe(s_priceFeedAddress);
+        deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -18,7 +21,7 @@ contract FuneMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
