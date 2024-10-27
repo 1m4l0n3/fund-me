be5f6163fb60357fef3740869939515c3c97439a
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-10-27 15:30:12 +0530

    [Johny] Add HelperConfiguration for Network related data

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+contract HelperConfig {
+    struct NetworkConfig {
+        address priceFeedAddress;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeedAddress: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+}
